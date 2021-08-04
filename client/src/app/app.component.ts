29f6e360a8ef1c488b4d0086c76e6d8d3848c423
ducvv <ducvv@ames.edu.vn>
2021-08-04 11:41:26 +0700

    update code

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+    title = 'App';
+    users: any;
+
+    constructor(private http: HttpClient) {
+
+    }
+    ngOnInit(): void {
+        this.getUsers();
+    }
+    getUsers() {
+        this.http.get('https://localhost:44394/api/users').subscribe(response => {
+            this.users = response;
+            console.log(this.users);
+        }, error => {
+        console.log(error);
+    })
+    }
+
+
+}
